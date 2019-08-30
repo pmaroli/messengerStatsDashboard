1567e8ad82b3fe9c1c19ba6b13f936b91143d518
pmaroli <pmaroli@DESKTOP-VJE6L5D.localdomain>
2019-08-30 16:27:10 -0700

    Updates

---
+++ precache-manifest.16f2003d63fbfa97dee092982c088e0f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "620be1ecc5c698732f11188bc0df5dfa",
+    "url": "/messengerStatsDashboard/index.html"
+  },
+  {
+    "revision": "f401d1b60a667b8be799",
+    "url": "/messengerStatsDashboard/static/css/main.937c13d4.chunk.css"
+  },
+  {
+    "revision": "69f8e069c60100b45846",
+    "url": "/messengerStatsDashboard/static/js/2.eb57c07c.chunk.js"
+  },
+  {
+    "revision": "f401d1b60a667b8be799",
+    "url": "/messengerStatsDashboard/static/js/main.61d870d2.chunk.js"
+  },
+  {
+    "revision": "086b63f87fc46ba2b7e3",
+    "url": "/messengerStatsDashboard/static/js/runtime~main.5bb46e53.js"
+  }
+]);
